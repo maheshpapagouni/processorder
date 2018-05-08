5f56d8b7dfd5d0386fc23f7cac56507c5d965a96
Mahesh <papagounimahesh@gmail.com>
2018-05-08 16:55:59 -0500

    updated

--- processorder/src/processorder/Order.java
+++ processorder/src/processorder/Order.java
@@ -2,34 +2,34 @@ package processorder;
 
 public class Order {
 int id;
-int fruit;
+int Quantity;
 
-public Order(int id, int fruit) {
+public Order(int id, int Quantity) {
 	super();
 
 	this.id = id;
-	this.fruit = fruit;
+	this.Quantity = Quantity;
 	
 }
  
-public void Apple(int OrdereApples ) 
+public void Apple(int OrdereApples) 
 {
-		if(id == 1 && OrdereApples >= this.fruit && fruit > 0){
-		OrdereApples = OrdereApples-this.fruit;	
+		if(id == 1 && OrdereApples >= this.Quantity && Quantity > 0){
+		OrdereApples = OrdereApples-this.Quantity;	
 	System.out.println(" Product Id : " +id );
 	System.out.println(" Product Name : Apple ");
 	System.out.println(" Product Description : Freshly Imported American Apples");
-	System.out.println(" No of Apples you Ordered : " + fruit);
+	System.out.println(" No of Apples you Ordered : " + Quantity);
 	System.out.println(" Price of Each Apple = $2.50 ");
-	System.out.println(" Price for Total Ordered Apples = $" + 2.50 * fruit);
+	System.out.println(" Price for Total Ordered Apples = $" + 2.50 * Quantity);
 	System.out.println(" No of Apples Left in Inventory : " + OrdereApples);
 	}
 		
-    else if (id ==1 && OrdereApples > this.fruit ){	
+    else if (id ==1 && OrdereApples > this.Quantity ){	
 	System.err.println("Please Input Proper Quantity Value");
 		}
 		
-	else if (id ==1 && OrdereApples < this.fruit) {
+	else if (id ==1 && OrdereApples < this.Quantity) {
 	System.err.println("Sorry, Order Cannot be Processed.");
 	System.out.println("Maximum Available Apples = " + OrdereApples);
 	}
@@ -37,23 +37,23 @@ public void Apple(int OrdereApples )
 
 public void orange(int OrderOranges ) {
 	
-	if(id == 2 && OrderOranges >= this.fruit && fruit > 0){
-		OrderOranges = OrderOranges-this.fruit;	
+	if(id == 2 && OrderOranges >= this.Quantity && Quantity > 0){
+		OrderOranges = OrderOranges-this.Quantity;	
 	System.out.println(" Product Id : " +id );
 	System.out.println(" Product Name : Orange ");
 	System.out.println(" Product Description : Best Grade Canada Oranges");
-	System.out.println(" No of Oranges you Ordered : " + fruit);
+	System.out.println(" No of Oranges you Ordered : " + Quantity);
 	System.out.println(" Price of Each Oranges = $4.50 ");
-	System.out.println(" Price for Total Ordered Apples = $" + 4.50 * fruit);
+	System.out.println(" Price for Total Ordered Apples = $" + 4.50 * Quantity);
 	System.out.println(" No of Oranges Left in Inventory : " + OrderOranges);
 	}
 
 
-	else if (id ==2 && OrderOranges > this.fruit ){	
+	else if (id ==2 && OrderOranges > this.Quantity ){	
 	System.err.println("Please Input Proper Quantity Value");
 		}
 		
-	else if (id ==2 && OrderOranges < this.fruit) {
+	else if (id ==2 && OrderOranges < this.Quantity) {
 	System.err.println("Sorry, Order Cannot be Processed.");
 	System.out.println("Maximum Available Oranges = " + OrderOranges);
 	}
@@ -61,18 +61,18 @@ public void orange(int OrderOranges ) {
 
 public void Mango(int OrderMangoes){
 	
-	if(id == 3 && OrderMangoes >= this.fruit && fruit > 0){
-		OrderMangoes = OrderMangoes-this.fruit;	
+	if(id == 3 && OrderMangoes >= this.Quantity && Quantity > 0){
+		OrderMangoes = OrderMangoes-this.Quantity;	
 	System.out.println(" Product Id : " +id );
 	System.out.println(" Product Name : Mango ");
 	System.out.println(" Product Description : Freshly Picked Indian Mangoes");
-	System.out.println(" No of Mangoes you Ordered : " + fruit);
+	System.out.println(" No of Mangoes you Ordered : " + Quantity);
 	System.out.println(" Price of Each Apple = $3.75 ");
-	System.out.println(" Price for Total Ordered Apples = $" + 3.75 * fruit);
+	System.out.println(" Price for Total Ordered Apples = $" + 3.75 * Quantity);
 	System.out.println(" Number of Mangoes Left in Inventory : " + OrderMangoes);
 	}
 
-else if (id ==3 && OrderMangoes > this.fruit){	
+else if (id ==3 && OrderMangoes > this.Quantity){	
 	System.err.println("Please Input Proper Quantity Value");
 		}
 	
@@ -81,7 +81,7 @@ else if (id ==3 && OrderMangoes > this.fruit){
 				System.out.println(" 1- Apple, 2- Orange, 3- Mango");
 	}
 		
-	else if (id ==3 && OrderMangoes < this.fruit) {
+	else if (id ==3 && OrderMangoes < this.Quantity) {
 	System.err.println("Sorry, Order Cannot be Processed.");
 	System.out.println("Maximum available Mangoes = " + OrderMangoes);
 	}
--- processorder/src/processorder/Product.java
+++ processorder/src/processorder/Product.java
@@ -4,15 +4,15 @@ public class Product extends Order{
 
 
 	
-	public Product(int id, int fruit) {
-		super(id, fruit);
+	public Product(int id, int Quantity) {
+		super(id, Quantity);
 		// TODO Auto-generated constructor stub
 	}
 
 	public static void main(String[] args) {
 		Order batch = new Order (1,10);
 
-		batch.Apple(21);
+		batch.Apple(20);
 		batch.orange(25);
 		batch.Mango(50);
 
